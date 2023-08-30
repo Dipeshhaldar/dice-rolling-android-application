dd693d4da6bc05f3bed9637b952ea2649e483dde
unknown <dipeshhaldar787@gmail.com>
2023-08-30 07:10:24 +0530

    Dice Rolling Android Application

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/gradient_color.dart';
+
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(
+            Color.fromARGB(255, 26, 2, 80), Color.fromARGB(255, 45, 7, 98)),
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
